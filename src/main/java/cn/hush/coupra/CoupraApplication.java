4ac04cae51a5dbfe2f2f0168acafa3d30a4f8d44
wanghongshen <wanghongshen@chuanyuapp.com>
2025-06-24 15:10:40 +0800

    初始化

---
+++ src/main/java/cn/hush/coupra/CoupraApplication.java
@@ -0,0 +1,13 @@
+package cn.hush.coupra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoupraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoupraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/hush/coupra/CoupraApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hush.coupra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoupraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
