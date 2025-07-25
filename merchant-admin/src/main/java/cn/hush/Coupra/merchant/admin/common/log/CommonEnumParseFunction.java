c54d6df1c5d8f91c7c42a03de8edb018904e371e
wanghongshen <wanghongshen@chuanyuapp.com>
2025-07-25 19:37:24 +0800

    log1

---
+++ merchant-admin/src/main/java/cn/hush/Coupra/merchant/admin/common/log/CommonEnumParseFunction.java
@@ -0,0 +1,23 @@
+package cn.hush.Coupra.merchant.admin.common.log;
+
+import com.mzt.logapi.service.IParseFunction;
+
+/**
+ * 操作日志组件解析枚举值对应描述信息
+ * <p>
+ * 开发时间：2024-07-09
+ */
+
+public class CommonEnumParseFunction implements IParseFunction {
+
+
+    @Override
+    public String functionName() {
+        return "";
+    }
+
+    @Override
+    public String apply(Object value) {
+        return "";
+    }
+}
