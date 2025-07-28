2dd13c928ffb8655745403b2a4d626086a78b540
wanghongshen <wanghongshen@chuanyuapp.com>
2025-07-28 19:34:37 +0800

    nodu2

--- framework/src/main/java/cn/hush/Coupra/framework/idempotent/NoDuplicateSubmitAspect.java
+++ framework/src/main/java/cn/hush/Coupra/framework/idempotent/NoDuplicateSubmitAspect.java
@@ -1,4 +1,21 @@
 package cn.hush.Coupra.framework.idempotent;
 
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.annotation.Aspect;
+import org.redisson.api.RedissonClient;
+
+/**
+ * 防止用户重复提交表单信息切面控制器
+ */
+@Aspect
+@RequiredArgsConstructor
 public class NoDuplicateSubmitAspect {
+
+    private final RedissonClient redissonClient;
+
+    /**
+     * 增强方法标记 {@link NoDuplicateSubmit} 注解逻辑
+     */
+
+
 }
