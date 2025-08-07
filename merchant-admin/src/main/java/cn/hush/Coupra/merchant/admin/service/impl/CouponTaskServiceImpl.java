c02114e84c3916af1d211c88fd1598065306702a
Purhooli <1422419663@qq.com>
2025-08-07 16:42:49 +0800

    thread-pool1

--- merchant-admin/src/main/java/cn/hush/Coupra/merchant/admin/service/impl/CouponTaskServiceImpl.java
+++ merchant-admin/src/main/java/cn/hush/Coupra/merchant/admin/service/impl/CouponTaskServiceImpl.java
@@ -15,11 +15,19 @@ import cn.hush.Coupra.merchant.admin.service.handler.excel.RowCountListener;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.IdUtil;
 import com.alibaba.excel.EasyExcel;
+import com.alibaba.fastjson2.JSONObject;
 import com.mybatisflex.spring.service.impl.ServiceImpl;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.redisson.api.RBlockingDeque;
+import org.redisson.api.RDelayedQueue;
+import org.redisson.api.RedissonClient;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Objects;
+import java.util.concurrent.*;
 
 /**
  * @program: Coupra
@@ -33,7 +41,30 @@ public class CouponTaskServiceImpl extends ServiceImpl<CouponTaskMapper, CouponT
 
     private final CouponTemplateService couponTemplateService;
     private final CouponTaskMapper couponTaskMapper;
+    private final RedissonClient redissonClient;
 
+    /**
+     * 为什么这里拒绝策略使用直接丢弃任务？因为在发送任务时如果遇到发送数量为空，会重新进行统计
+     */
+    private final ExecutorService executorService = new ThreadPoolExecutor(
+            Runtime.getRuntime().availableProcessors(),
+            Runtime.getRuntime().availableProcessors() << 1,
+            60,
+            TimeUnit.SECONDS,
+            new SynchronousQueue<>(),
+            new ThreadPoolExecutor.DiscardPolicy()
+    );
+
+    /**
+     * 因为静态内部类的 Bean 注入有问题，所以我们这里直接 new 对象运行即可
+     * 如果按照上一版本的方式写，refreshCouponTaskSendNum 方法中 couponTaskMapper 为空
+     */
+    @PostConstruct
+    public void init() {
+        new RefreshCouponTaskDelayQueueRunner(this, couponTaskMapper, redissonClient).run();
+    }
+
+    @Transactional(rollbackFor = Exception.class)
     @Override
     public void createCouponTask(CouponTaskCreateReqDTO requestParam) {
         // 验证非空参数
@@ -56,16 +87,76 @@ public class CouponTaskServiceImpl extends ServiceImpl<CouponTaskMapper, CouponT
                         : CouponTaskStatusEnum.PENDING.getStatus()
         );
 
-        // 通过 EasyExcel 监听器获取 Excel 中所有行数
-        RowCountListener listener = new RowCountListener();
-        EasyExcel.read(requestParam.getFileAddress(), listener).sheet().doRead();
-
-        // 为什么需要统计行数？因为发送后需要比对所有优惠券是否都已发放到用户账号
-        int totalRows = listener.getRowCount();
-        couponTaskDO.setSendNum(totalRows);
-
         // 保存优惠券推送任务记录到数据库
         couponTaskMapper.insert(couponTaskDO);
+
+        // 为什么需要统计行数？因为发送后需要比对所有优惠券是否都已发放到用户账号
+        // 100 万数据大概需要 4 秒才能返回前端，如果加上验证将会时间更长，所以这里将最耗时的统计操作异步化
+        JSONObject delayJsonObject = JSONObject
+                .of("fileAddress", requestParam.getFileAddress(), "couponTaskId", couponTaskDO.getId());
+        executorService.execute(() -> refreshCouponTaskSendNum(delayJsonObject));
+
+        // 假设刚把消息提交到线程池，突然应用宕机了，我们通过延迟队列进行兜底 Refresh
+        RBlockingDeque<Object> blockingDeque = redissonClient.getBlockingDeque("COUPON_TASK_SEND_NUM_DELAY_QUEUE");
+        RDelayedQueue<Object> delayedQueue = redissonClient.getDelayedQueue(blockingDeque);
+        // 这里延迟时间设置 20 秒，原因是我们笃定上面线程池 20 秒之内就能结束任务
+        delayedQueue.offer(delayJsonObject, 20, TimeUnit.SECONDS);
     }
+
+    private void refreshCouponTaskSendNum(JSONObject delayJsonObject) {
+        // 通过 EasyExcel 监听器获取 Excel 中所有行数
+        RowCountListener listener = new RowCountListener();
+        EasyExcel.read(delayJsonObject.getString("fileAddress"), listener).sheet().doRead();
+        int totalRows = listener.getRowCount();
+
+        // 刷新优惠券推送记录中发送行数
+        CouponTaskDO updateCouponTaskDO = CouponTaskDO.builder()
+                .id(delayJsonObject.getLong("couponTaskId"))
+                .sendNum(totalRows)
+                .build();
+        couponTaskMapper.update(updateCouponTaskDO);
+    }
+
+
+    /**
+     * 优惠券延迟刷新发送条数兜底消费者｜这是兜底策略，一般来说不会执行这段逻辑
+     * 如果延迟消息没有持久化成功，或者 Redis 挂了怎么办？后续可以人工处理
+     */
+     @RequiredArgsConstructor
+     static class RefreshCouponTaskDelayQueueRunner{
+
+        private final CouponTaskServiceImpl couponTaskService;
+        private final CouponTaskMapper couponTaskMapper;
+        private final RedissonClient redissonClient;
+
+        public void run() {
+            Executors.newSingleThreadExecutor(
+                    runnable -> {
+                        Thread thread = new Thread(runnable);
+                        thread.setName("delay_coupon-task_send-num_consumer");
+                        thread.setDaemon(Boolean.TRUE);
+                        return thread;
+            })
+            .execute(() -> {
+                RBlockingDeque<JSONObject> blockingDeque = redissonClient.getBlockingDeque("COUPON_TASK_SEND_NUM_DELAY_QUEUE");
+                for (; ; ) {
+                    try {
+                         // 获取延迟队列已到达时间元素
+                        JSONObject delayJsonObject = blockingDeque.take();
+                        if (delayJsonObject != null) {
+                            // 获取优惠券推送记录，查看发送条数是否已经有值，有的话代表上面线程池已经处理完成，无需再处理
+                            CouponTaskDO couponTaskDO = couponTaskMapper.selectOneById(delayJsonObject.getLong("couponTaskId"));
+                            if (couponTaskDO.getSendNum() == null) {
+                                couponTaskService.refreshCouponTaskSendNum(delayJsonObject);
+                            }
+                        }
+                    } catch (Throwable ignored) {
+                    }
+                }
+            });
+        }
+    }
+
+
 
 }
